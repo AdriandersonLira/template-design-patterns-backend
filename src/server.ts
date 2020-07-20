b1dcf5accd2133b946a9e9672b668078740ca657
Adrianderson Lira <adriandersonmusica@gmail.com>
2020-07-20 17:24:10 -0300

    add middleware logRequest

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import console from 'console';
 import routes from './routes';
+import logRequest from './middlewares/logRequests.middleware';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(logRequest);
 
 app.use(routes);
 
